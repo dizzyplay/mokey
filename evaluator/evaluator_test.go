060eb22b641c30148ca17c63f5430796381a493d
cybaek <vovoboss@gmail.com>
2023-12-25 22:14:47 +0900

    bool

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -17,8 +17,23 @@ func TestEvalIntergerExpression(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		evaludated := testEval(tt.input)
-		testIntegerObject(t, evaludated, tt.expected)
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
 
@@ -27,6 +42,18 @@ func testEval(input string) object.Object {
 	p := parser.New(l)
 	program := p.ParseProgram()
 	return Eval(program)
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("obj is not Boolean got=%T (%+v)", obj, obj)
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t,want=%t", result.Value, expected)
+		return false
+	}
+	return true
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
