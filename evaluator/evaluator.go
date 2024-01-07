eafd2b453c217bd05a58f0619a0b6af25b9fb450
cybaek <vovoboss@gmail.com>
2024-01-07 23:21:16 +0900

    resolved nested block statemnet

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,9 +14,9 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 	case *ast.IfExpresion:
 		return evalIfExpression(node)
 	case *ast.InfixExpression:
@@ -37,6 +37,32 @@ func Eval(node ast.Node) object.Object {
 		return &object.ReturnValue{Value: val}
 	}
 	return nil
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+	return result
+}
+
+func evalProgram(program *ast.Program) object.Object {
+	var result object.Object
+
+	for _, statement := range program.Statements {
+		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+	return result
 }
 
 func evalIfExpression(ie *ast.IfExpresion) object.Object {
@@ -107,17 +133,6 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
-}
-
-func evalStatements(stmts []ast.Statement) object.Object {
-	var result object.Object
-	for _, statement := range stmts {
-		result = Eval(statement)
-		if returnValue, ok := result.(*object.ReturnValue); ok {
-			return returnValue.Value
-		}
-	}
-	return result
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
