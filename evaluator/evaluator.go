2c417524fbd46d68a72a768453846527c02fe84c
cybaek <vovoboss@gmail.com>
2024-01-12 22:59:34 +0900

    add function test

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,6 +16,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalProgram(node, env)
+	case *ast.FunctionLiteral:
+		return &object.Function{Parameters: node.Paramaters, Body: node.Body}
 	case *ast.LetStatement:
 		val := Eval(node.Value, env)
 		if isError(val) {
