f72bf2a46565baeac6334d5c65359063c59cc7ea
cybaek <vovoboss@gmail.com>
2024-01-14 16:26:15 +0900

    delete fmt

--- object/environment.go
+++ object/environment.go
@@ -1,7 +1,5 @@
 package object
 
-import "fmt"
-
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s, outer: nil}
@@ -14,7 +12,6 @@ type Environment struct {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
-	fmt.Println(e)
 	if !ok && e.outer != nil {
 		obj, ok = e.outer.Get(name)
 	}
